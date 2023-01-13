799caf5d2a55fed460b0ffabc3a9d39bb22e35ea
hkang4628 <hkang4628@gmail.com>
2023-01-13 14:19:08 +0900

    pickle 파일을 사용하여 크롤링 여부 결정 기능 추가

---
+++ scrap/data_control.py
@@ -0,0 +1,29 @@
+import pickle
+import pandas as pd
+
+
+# csv 파일의 내용 읽어오기
+def get_url(name: str) -> str:
+    csv = 'scrap/url.csv'
+    df = pd.read_csv(csv, encoding='UTF-8', header=0, index_col=0)
+    return df.loc[name, "url"]
+
+# 스크랩한 데이터 리스트를 pickle로 저장
+
+
+def save_list(name: str, data_list: list) -> None:
+    try:
+        with open(f'scrap/scraped_list/{name}.p', 'wb') as f:
+            pickle.dump(data_list, f)
+        print(f'{name} 저장 성공!')
+
+    except Exception as e:
+        print(f'{name} 저장 실패!')
+        print(e)
+
+# pcikle로 저장된 리스트를 불러옴
+
+
+def load_list(name: str):
+    with open(f'scrap/scraped_list/{name}.p', 'rb') as f:
+        return pickle.load(f)
--- scrap/scraper/get_pantheons.py
+++ scrap/scraper/get_pantheons.py
@@ -1,27 +1,45 @@
+import data_control
+import os
 import time
 
 from selenium import webdriver  # 웹 브라우저 컨트롤을 위한 프레임 워크
 from selenium.webdriver.common.by import By  # selenium 4.0 이상에서 사용
 
-pantheon = []
 
-def get_data(url: str) -> list:
-    # Chrome Driver 위치
-    cd = 'C:/Users/Administrator/AppData/Local/Google/Chrome/chromedriver.exe'
+def get_data(name: str) -> list:
+    pantheon = []
 
-    # web 페이지 로딩을 기다리는 시간
-    wait_loading = 3
-
-    # 웹 드라이버 생성
-    with webdriver.Chrome(cd) as driver:
-        driver.get(url)
-        time.sleep(wait_loading)
-
-        # 판테온 NAME 테이블 선택
-        tr_list = driver.find_elements(
-            By.XPATH, '//*[@id="Pantheonlists"]/div/table/tbody/tr')
-        for tr in tr_list:
-            td_list = tr.find_elements(By.TAG_NAME, 'td')
-            pantheon.append(td_list[1].text)
+    if os.path.isfile(f'scrap/scraped_list/{name}.p'):
+        # {name} pickle 파일이 있을 시 pickle load
+        pantheon = data_control.load_list(name)
         
+    else:
+        # {name} pickle 파일이 없으면 크롤링
+
+        # url 가져오기
+        url = data_control.get_url(name)
+
+        # 크롤링
+        # Chrome Driver 위치
+        cd = 'C:/Users/Administrator/AppData/Local/Google/Chrome/chromedriver.exe'
+
+        # web 페이지 로딩을 기다리는 시간
+        wait_loading = 3
+
+        # 웹 드라이버 생성
+        with webdriver.Chrome(cd) as driver:
+            driver.get(url)
+            time.sleep(wait_loading)
+
+            # 판테온 NAME 테이블 선택
+            tr_list = driver.find_elements(
+                By.XPATH, '//*[@id="Pantheonlists"]/div/table/tbody/tr')
+            for tr in tr_list:
+                td_list = tr.find_elements(By.TAG_NAME, 'td')
+                pantheon.append(td_list[1].text)
+            
     return pantheon
+
+
+
+    
