60b90b08afeeb334aaa397b169c11a13dca6c454
hkang4628 <hkang4628@gmail.com>
2023-01-16 11:33:45 +0900

    add classes code

--- scrap/main.py
+++ scrap/main.py
@@ -19,14 +19,27 @@ if __name__ == '__main__':
     print(df)
 
     # DB에 저장
+
     # ==================================
-    # 판테온 데이터 스크랩
+    # NPC and Object 데이터 스크랩
     name = "objects"
     df = get_objects.get_data(name)
     print(df)
 
     # 초성 Column 추가
     df['first_letter'] = df.apply(data_control.add_first_letter, axis=1)
+
+    # DB에 저장
+
+    # ==================================
+    # 직업 데이터 스크랩
+    name = "classes"
+    df = get_classes.get_data(name)
+    print(df)
+
+    # 초성 Column 추가
+    df['first_letter'] = df.apply(data_control.add_first_letter, axis=1)
+    print(df.head(15))
     print(df.tail(15))
 
     # DB에 저장
